c61f1f19b0fca7a89baf96534fa2d172de7b5752
kamiokan <spherenov@gmail.com>
2019-09-16 10:09:08 +0900

    c++でint,long,long longの最大値を表示する

---
+++ check-climits.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <climits>
+
+using namespace std;
+
+// int, long, long long の最大値を表示する
+int main() {
+    cin.tie(0);
+    ios::sync_with_stdio(false);
+
+    cout << INT_MAX << endl;
+    cout << LONG_MAX << endl;
+    cout << LLONG_MAX << endl;
+
+    return 0;
+}
